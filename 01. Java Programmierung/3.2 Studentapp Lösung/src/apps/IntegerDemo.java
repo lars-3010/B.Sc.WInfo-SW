411551273615e1f4ba02d5fd4105381af4df52fc
Lars Boes <iLars.Boes@gmail.com>
2024-06-28 10:32:08 +0200

    formatierung

--- 01. Java Programmierung/3.2 Studentapp Lösung/src/apps/IntegerDemo.java
+++ 01. Java Programmierung/3.2 Studentapp Lösung/src/apps/IntegerDemo.java
@@ -2,12 +2,15 @@ package apps;
 import utilities.Console;
 
 public class IntegerDemo {
-public static void main(String[] args) {
-int i = 123;
-Integer j = Integer.valueOf(123);
-String integerAsString = Console.readString("Bitte eine ganze Zahl eingeben");
-Integer integerValue = Integer.valueOf(integerAsString);
-int intValueResult = integerValue.intValue();
-Console.println("Der Zahlenwert ist: " + intValueResult);
-}
+    public static void main(String[] args) {
+        int i = 123;
+        Integer j = Integer.valueOf(123);
+
+        String integerAsString = Console.readString("Bitte eine ganze Zahl eingeben");
+
+        Integer integerValue = Integer.valueOf(integerAsString);
+        int intValueResult = integerValue.intValue();
+        
+        Console.println("Der Zahlenwert ist: " + intValueResult);
+    }
 }
