e20798fc4855c49e8c7fe6ecc4eec0e63a5e68ee
Ayoub Mahbud Arrousou <mahbudarrousouayoub@gmail.com>
2024-07-02 11:16:20 +0200

    enum

---
+++ 01. Java Programmierung/3.2 Studentapp Lösung/src/model/StudentCollection.java
@@ -0,0 +1,18 @@
+package model;
+import utilities.IdFactory;
+
+public class StudentCollection {
+    private Student[] students;
+    public StudentCollection() {
+        students = new Student[IdFactory.LAST_ID];
+    }
+    public String toString() {
+String result = "Currently available students are:";
+    for (Student student : students) {
+        if (student != null) {
+            result += "\n - " + student.toString();
+        }
+    }
+    return result;
+}
+}
---
+++ 01. Java Programmierung/3.2 Studentapp Lösung/src/model/Weekday.java
@@ -0,0 +1,11 @@
+package model;
+
+public enum Weekday {
+    MONDAY, 
+    TUESDAY, 
+    WEDNESDAY, 
+    THURSDAY, 
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+}
