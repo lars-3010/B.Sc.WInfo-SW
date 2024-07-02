4d6520ef832e35afb43ccbdfa072797a512655e2
Lars Boes <iLars.Boes@gmail.com>
2024-07-02 12:00:08 +0200

    neustart :)

---
+++ 01. Java Programmierung/Vorlesung/src/apps/WeekdayDemo.java
@@ -0,0 +1,16 @@
+package apps;
+
+import model.Weekday;
+ 
+public class WeekdayDemo {
+ 
+	public static void main(String[] args) {
+		Weekday day=Weekday.TUESDAY;
+		System.out.println(day1);
+        System.out.println(day1.getGermanName());
+        System.out.println(day1.getDayNumber());
+        System.out.println(day1.ordinal());
+ 
+	}
+ 
+}
